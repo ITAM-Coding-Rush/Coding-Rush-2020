86730e4630a3cc0efa88e65cf5bb0c4e51d265d0
Juan Carlos Sigler Priego <jcsiglerp@gmail.com>
2020-10-06 11:42:59 -0500

    Fix solutionB

--- semana1/calificando-examenes/solutions/solutionB.py
+++ semana1/calificando-examenes/solutions/solutionB.py
@@ -2,6 +2,6 @@ n = int(input())
 materia = input()
 A = []
 for i in range(n):
-    A += float(input())
+    A += [float(input())]
 
 print("El promedio del examen de la materia {} es {}".format(materia, str(sum(A) / n)))
