505396ba5198f4caa5ab94399fa9acb0a5f3f863
AlexanderOG <alexander_obeid@hotmail.com>
2020-10-14 18:50:06 -0500

    Quite casos grandes

--- semana2/mascotas/case-generator.py
+++ semana2/mascotas/case-generator.py
@@ -4,16 +4,10 @@ cases=20
 for i in range(cases):
     nombre = "./cases/" + str(i)+".in"
     f = open(nombre, "w")
-    if i < 15:
-        n = r.randint(10,30)
-    else:
-        n = r.randint(60,100)
+    n = r.randint(10,30)
     f.write(str(n) + "\n")
     for k in range(n):
-        if i < 15:
-            x = r.randint(1,10)
-        else:
-            x = r.randint(20000,100000)
+        x = r.randint(1,10)
         b = ''
         for j in range(x):
             b+= alfa[r.randint(0,len(alfa)-1)]
