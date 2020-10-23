7718282a3d40af8110c9ba8bbe29b0a48e320d28
CaDe27 <cdelgadoelizondo@gmail.com>
2020-10-23 11:59:47 -0500

    Problema de la semana 3: Ayudando a Adriana - Carlos Delgado

---
+++ semana3/SOSAyudandoAdriana/statements/plantilla.py
@@ -0,0 +1,21 @@
+n = 
+for :
+  
+m = 
+for :
+
+candidatos = 
+s = 
+for i :
+  camposImportantesLlenados = 
+  camposLlenados = 
+
+  for j :
+    if():
+      camposImportantesLlenados = 
+      
+  if():
+    candidatos = 
+
+
+print()
